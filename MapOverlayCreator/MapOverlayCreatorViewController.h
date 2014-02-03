719fd6c6347b7f59c086f9c558389aa79eafa443
Philipp Häfele <ii12015@nta-isny.de>
2014-02-03 11:01:04 +0100

    Added some more views and added a property
    Property is needed because mapView doesn't save annotations ordered. so
    we need an nsmutablearray.

--- MapOverlayCreator/MapOverlayCreatorViewController.h
+++ MapOverlayCreator/MapOverlayCreatorViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
 @interface MapOverlayCreatorViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
--- MapOverlayCreator/MapOverlayCreatorViewController.m
+++ MapOverlayCreator/MapOverlayCreatorViewController.m
@@ -10,6 +10,9 @@
 
 @interface MapOverlayCreatorViewController ()
 
+// Need this property because the annoataion array in mapView is unsorted
+@property (nonatomic, strong) NSMutableArray *polyPoints;
+
 @end
 
 @implementation MapOverlayCreatorViewController
@@ -18,12 +21,128 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Set maptype to hybrid so we can see the buildings and also can orientate oneself to roads etc.
+    self.mapView.mapType = MKMapTypeHybrid;
+    
+    // Show userlocation
+    //self.mapView.showsUserLocation = YES;
+    
+#warning change this to something better or let the user save an own location
+    // create and set start Locarion
+    CLLocationCoordinate2D startCoordinate = CLLocationCoordinate2DMake(47.693888, 10.037259);
+    
+    self.mapView.region = MKCoordinateRegionMakeWithDistance(startCoordinate, 100, 100);
+    
+    // Create a gesture recognizer for long presses
+    UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
+    // Set minimum press time to half a second
+    longPress.minimumPressDuration = 0.3;
+    // Add gesture recognizer to mapView
+    [self.mapView addGestureRecognizer:longPress];
 }
 
-- (void)didReceiveMemoryWarning
+// Method that creates the OverlayView
+- (MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id <MKOverlay>)overlay
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if ([overlay isKindOfClass:[MKPolygon class]])
+    {
+        MKPolygonView *polyView = [[MKPolygonView alloc] initWithPolygon:(MKPolygon *)overlay];
+        
+        polyView.fillColor = [[UIColor blueColor] colorWithAlphaComponent:0.5];
+        polyView.strokeColor = [UIColor blueColor];
+        polyView.lineWidth = 3;
+        
+        return polyView;
+    }
+    return nil;
+}
+
+
+// Gesture recognizers method for the long press on the mapView
+- (void)handleLongPress:(UIGestureRecognizer *)gestureRecognizer
+{
+    if (gestureRecognizer.state == UIGestureRecognizerStateBegan)
+    {
+        // Get the touch position / point in the mapView
+        CGPoint touchPoint = [gestureRecognizer locationInView:self.mapView];
+        
+        // Get the coordinates of the tapped point
+        CLLocationCoordinate2D touchMapCoordinate = [self.mapView convertPoint:touchPoint toCoordinateFromView:self.mapView];
+        
+        // Create an annotaion and set the coordinate property
+        MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+        
+        point.coordinate = touchMapCoordinate;
+        
+        // Add the annotation to the polyPoints array
+        [self.polyPoints addObject:point];
+        
+        // Add the annotation to the map
+        [self.mapView addAnnotation:point];
+    }
+}
+
+// Method to delete all annotations from the mapView
+- (IBAction)deleteAnnotations:(UIBarButtonItem *)sender
+{
+    for (id annotation in self.mapView.annotations)
+    {
+        if ([annotation isKindOfClass:[MKPointAnnotation class]])
+        {
+            [self.mapView removeAnnotation:annotation];
+        }
+    }
+}
+
+// Function to create an overlay out of all current annotations in polyPoints (= mapView)
+- (IBAction)createOverlay:(UIBarButtonItem *)sender
+{
+    NSUInteger counter = 0;
+    
+    // Get the number of items (MKPointAnnotations) in the polyPoints array
+    NSUInteger numOfPoints = [self.polyPoints count];
+    
+    // Define an overlay th
+    CLLocationCoordinate2D  points[numOfPoints];
+    
+    for (id annotation in self.polyPoints)
+    {
+        if ([annotation isKindOfClass:[MKPointAnnotation class]])
+        {
+            MKPointAnnotation *point = (MKPointAnnotation *)annotation;
+            
+            points[counter] = point.coordinate;
+            
+            counter++;
+        }
+    }
+    
+    MKPolygon* poly = [MKPolygon polygonWithCoordinates:points count:numOfPoints];
+    
+    poly.title = @"Poly 1";
+    
+    
+    [self deleteAnnotations:nil];
+    
+    [self.mapView addOverlay:poly];
+}
+
+- (IBAction)delteOverlays:(UIBarButtonItem *)sender
+{
+    [self.mapView removeOverlays:self.mapView.overlays];
+}
+
+//################### Lazy instantiation ###################
+
+-(NSMutableArray *)polyPoints
+{
+    if (!_polyPoints)
+    {
+        _polyPoints = [[NSMutableArray alloc]init];
+    }
+    
+    return _polyPoints;
 }
 
 @end
